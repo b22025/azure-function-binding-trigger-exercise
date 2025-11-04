bb736954c7ce0ddb4c40b4f9f93b59b070834ceb
b22025 <kbuoy31@gmail.com>
2025-11-04 12:35:58 +0900

    Azure 함수앱과 이벤트 허브 트리거와 바인딩 실습 코드 추가

---
+++ function_app.py
@@ -0,0 +1,34 @@
+import azure.functions as func
+import logging
+
+app = func.FunctionApp()
+
+@app.event_hub_message_trigger(arg_name="azeventhub", event_hub_name="test-hub",
+                               connection="dt003eventhub_RootManageSharedAccessKey_EVENTHUB") 
+def eventhub_trigger(azeventhub: func.EventHubEvent):
+    logging.info('Python EventHub trigger processed an event: %s',
+                azeventhub.get_body().decode('utf-8'))
+
+@app.function_name(name="eventhub_output")
+@app.route(route="eventhub_output",methods=["POST"])
+@app.event_hub_output(arg_name="event", event_hub_name="test-hub3",
+                      connection="dt003eventhub_RootManageSharedAccessKey_EVENTHUB")
+def event_hub_output(req:func.HttpRequest, event: func.Out[str]) -> func.HttpResponse:
+    req_body=req.get_body().decode('utf-8')
+    logging.info("Http trigger function received a request : %s",req_body)
+    event.set(req_body)
+    return func.HttpResponse("success", status_code=200)
+
+# This example uses SDK types to directly access the underlying EventData object provided by the Event Hubs trigger.
+# To use, uncomment the section below and add azurefunctions-extensions-bindings-eventhub to your requirements.txt file
+# Ref: aka.ms/functions-sdk-eventhub-python
+#
+# import azurefunctions.extensions.bindings.eventhub as eh
+# @app.event_hub_message_trigger(
+#     arg_name="event", event_hub_name="test-hub", connection="dt003eventhub_RootManageSharedAccessKey_EVENTHUB"
+# )
+# def eventhub_trigger(event: eh.EventData):
+#     logging.info(
+#         "Python EventHub trigger processed an event %s",
+#         event.body_as_str()
+#     )
